aad41977372f893d2fbea237845e8fabe1aaa993
Khokon-Chandra <khokonchandra4@gmail.com>
2023-10-26 20:15:18 +0600

    fixed test code

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'active' => true,
             'username' => fake()->userName(),
-            'password' => Hash::make(12345678), // password
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
     }
