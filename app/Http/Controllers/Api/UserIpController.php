4bbc931ca9a8beed3cfbb2cd1df264db2c2baafe
MunnaHasa4 <munnahaasan.cikatech@gmail.com>
2023-10-29 21:46:23 +0600

    CKNT-59-cikanationapi-user-ip-update

--- app/Http/Controllers/Api/UserIpController.php
+++ app/Http/Controllers/Api/UserIpController.php
@@ -70,7 +70,7 @@ class UserIpController extends Controller
                 if (in_array($ip_address, $checkIps)) {
                     return response()->json([
                         'status' => 'error',
-                        'message' => 'Admin Ip already exist',
+                        'message' => 'User Ip already exist',
                         'ip_whitelist' => $ip_address,
                     ], 400);
                 }
@@ -85,14 +85,14 @@ class UserIpController extends Controller
                 'created_at' => now(),
             ];
 
-            $model = UserIp::create($payload);
+            $UserIp = UserIp::create($payload);
 
             // Create Activity Log
             activity('create user ip')->causedBy(Auth::user()->id)
-                ->performedOn($model)
+                ->performedOn($UserIp)
                 ->withProperties([
                     'ip' => Auth::user()->last_login_ip,
-                    'target' => $model->ip_address,
+                    'target' => $UserIp->ip_address,
                     'activity' => 'Create user ip',
                 ])
                 ->log('Successfully');
@@ -102,8 +102,117 @@ class UserIpController extends Controller
             return response()->json([
                 'status' => 'successful',
                 'message' => 'User Ip Created Successfully',
-                'data' => $model,
+                'data' => $UserIp,
             ],200);
+
+        } catch (\Exception$e) {
+            Log::error($e);
+            throw ValidationException::withMessages([$e->getMessage()]);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Validation
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'number1' => 'required|min:1|max:255|numeric',
+                'number2' => 'required|min:0|max:255|numeric',
+                'number3' => 'required|min:0|max:255|numeric',
+                'number4' => 'required|min:0|max:255|numeric',
+                'description' => 'required',
+            ],
+            [
+                'number1.required' => 'The number 1 IP is required',
+                'number2.required' => 'The number 2 IP is required',
+                'number3.required' => 'The number 3 IP is required',
+                'number4.required' => 'The number 4 IP is required',
+                '*.min' => 'The number ip must be at least 0.',
+                '*.max' => 'The number ip must not be greater than 255',
+            ]
+        );
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()->first(),
+            ], 400);
+        }
+        if ($request->number3 === null && $request->number4 !== null) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Format IP Invalid!.',
+            ], 400);
+        }
+
+        DB::beginTransaction();
+        try {
+
+            $UserIp = UserIp::find($id);
+
+            if (!$UserIp) {
+                throw ValidationException::withMessages(["$UserIp Ip Not Found"]);
+            }
+
+            $ip1 = $request->number1;
+            $ip2 = $request->number2;
+            $ip3 = $request->number3;
+            $ip4 = $request->number4;
+            $ip_address = $ip1 . '.' . $ip2 . '.' . $ip3 . '.' . $ip4;
+            $checkIps = UserIp::select('ip_address')->where('ip_address', 'LIKE', '%' . $ip1 . '.' . $ip2 . '%')->whereNotIn('id', [$id])->pluck('ip_address')->toArray();
+            if ($checkIps != []) {
+
+                if (in_array($ip_address, $checkIps)) {
+                    return response()->json([
+                        'status' => 'error',
+                        'message' => 'User Ip already exist',
+                        'ip_whitelist' => $ip_address,
+                    ], 400);
+                }
+            }
+
+            // Update on Database
+            $description = $request->description ?? $UserIp->description;
+            $payload = [
+                'ip_address' => $ip_address,
+                'whitelisted' => $request->whitelisted,
+                'description' => $description,
+                'updated_by' => auth()->user()->id,
+                'updated_at' => now(),
+            ];
+
+            $dataUpdate = [];
+            if ($UserIp->ip_address != $ip_address) {
+                $dataUpdate['ip_address'] = 'IP Address : ' . $UserIp->ip_address . ' -> ' . $ip_address;
+            }
+            if ($UserIp->whitelisted != $request->whitelisted) {
+                $dataUpdate['whitelisted'] = 'Whitelisted : ' . ($UserIp->whitelisted == 1 ? 'True' : 'False') . ' -> ' . ($request->whitelisted == 1 ? 'True' : 'False');
+            }
+            if ($UserIp->description != $description) {
+                $dataUpdate['description'] = 'Description : ' . $UserIp->description . ' -> ' . $description;
+            }
+
+            $dataLog = implode(', ', $dataUpdate) == null ? 'No Data Updated' : implode(', ', $dataUpdate);
+
+            $UserIp->update($payload);
+
+            // Create Activity Log
+            activity('update User ip')->causedBy(Auth::user()->id)
+                ->performedOn($UserIp)
+                ->withProperties([
+                    'ip' => Auth::user()->last_login_ip,
+                    'target' => $UserIp->ip_address,
+                    'activity' => 'Updated User ip',
+                ])
+                ->log('Successfully Updated User ip, ' . $dataLog);
+
+            DB::commit();
+
+            return response()->json([
+                'status' => 'successful',
+                'message' => 'User Ip Updated Successfully',
+                'data' => $UserIp,
+            ]);
 
         } catch (\Exception$e) {
             Log::error($e);
--- tests/Feature/Ip/UserIpTest.php
+++ tests/Feature/Ip/UserIpTest.php
@@ -28,7 +28,7 @@ class UserIpTest extends TestCase
 
             UserIp::create([
                 'ip_address' => '103.15.245.75',
-                'description' => 'munna Ip',
+                'description' => 'testing Ip',
                 'whitelisted' => 1,
                 'created_by' => 2,
             ]);
@@ -80,6 +80,63 @@ class UserIpTest extends TestCase
                 "created_by",
                 "created_at",
                 "id",
+            ]
+        ]);
+
+        $response->assertJson([
+            'status' => true,
+            'message' => true,
+            'data' => true
+        ]);
+    }
+
+    /**
+     * User Ip Update
+     */
+    public function test_userIpUpdate(): void
+    {
+        $this->artisan('migrate:fresh --seed');
+
+        $user = User::factory()
+            ->state([
+                'active' => true
+            ])
+            ->createQuietly();
+
+
+           $userIp = UserIp::create([
+                'ip_address' => '103.15.245.74',
+                'whitelisted' => 1,
+                'description' => 'testing ip update',
+                'created_by' => 1,
+                'created_at' => now(),
+            ]);
+
+
+        $response = $this->actingAs($user)->putJson('/api/v1/user-ip/'.$userIp->id.'', [
+            'number1' => 103,
+            'number2' => 15,
+            'number3' => 245,
+            'number4' => 75,
+            'description' => 'testing updated descriptoin',
+        ]);
+
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            "status",
+            "message",
+            "data" => [
+                "id",
+                "ip_address",
+                "description",
+                "whitelisted",
+                "created_by",
+                "updated_by",
+                "deleted_by",
+                "deleted_at",
+                "created_at",
+                "updated_at",
             ]
         ]);
 
