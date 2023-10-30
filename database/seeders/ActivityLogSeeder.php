f020d172a266c0ccb43d407e857174bad1b92da5
Khokon-Chandra <khokonchandra4@gmail.com>
2023-10-30 23:04:25 +0600

    remove activity.sql file

--- database/seeders/ActivityLogSeeder.php
+++ database/seeders/ActivityLogSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,7 +14,14 @@ class ActivityLogSeeder extends Seeder
      */
     public function run(): void
     {
-        $contents = file_get_contents(storage_path('activity.sql'));
-        DB::unprepared($contents);
+        $user = User::first();
+        activity('User Login')->causedBy($user->id)
+            ->performedOn($user)
+            ->withProperties([
+                'ip' => '127.0.0.1',
+                'target' => $user->username,
+                'activity' => 'Dummy test activity',
+            ])
+            ->log('Dummy test activity');
     }
 }
