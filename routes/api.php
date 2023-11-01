47760fbb855d26969220803a4a44ba20e093adde
MunnaHasa4 <munnahaasan.cikatech@gmail.com>
2023-11-01 21:08:18 +0600

    CKNT-37-cikanationapi-user-create

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AnnouncementController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\RoleController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,4 +14,10 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => ['auth:api']], function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::resource('roles',RoleController::class);
+    Route::resource('user',UserController::class);
+
+    Route::get('announcements',[AnnouncementController::class,'index'])->name('announcements.index');
+    Route::post('announcements',[AnnouncementController::class,'store'])->name('announcements.store');
+    Route::put('announcements',[AnnouncementController::class,'update'])->name('announcements.update');
+    Route::delete('announcements',[AnnouncementController::class,'destroy'])->name('announcements.destroy');
 });
