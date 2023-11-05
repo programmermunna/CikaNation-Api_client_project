637fac9381bef9fc0e234ef5f27a875f803d23fe
Khokon-Chandra <khokonchandra4@gmail.com>
2023-11-05 11:19:00 +0600

    rollback primary key conf

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -12,8 +11,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-        DB::statement('SET SESSION sql_require_primary_key=0');
-        
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')
                 ->primary()
