8417a4aba4a7d346df7e7352ca30cc5c7942623b
Khokon-Chandra <khokonchandra4@gmail.com>
2023-11-05 11:14:45 +0600

    set mysql primary key disabled set on create table event session

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,6 +12,8 @@ return new class extends Migration
      */
     public function up(): void
     {
+        DB::statement('SET SESSION sql_require_primary_key=0');
+        
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')
                 ->primary()
